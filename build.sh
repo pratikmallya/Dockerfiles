9e267b2cc3bf249e7f8c6d87f610942394bb8bde
Pratik Mallya <pratik.mallya@gmail.com>
2018-09-20 20:13:09 -0700

    Remove unused build.sh script (#10)
    Its included in the Makefile

--- build.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-dockerfiles=$(ls | grep "Dockerfile.*")
-	for dockerfile in ${dockerfiles}; do
-		tag=$(echo "$dockerfile" | cut -d '.' -f2)
-		docker build -f $dockerfile . -t "${DHUSERNAME}/${tag}"
-done
